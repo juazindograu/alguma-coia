65558bbfb98d57d5783954bed7344014d9dad57f
juazindograu <160233841+juazindograu@users.noreply.github.com>
2024-11-26 12:02:27 -0300

    Add files via upload

---
+++ alguma-coia-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'esportes',
+    'quem é o homem mais rapido do mundo?',
+    'usain bolt'
+)
+
+criaCartao(
+    'animal',
+    'qual o animal mais inutil do mundo?',
+    'o preixe lua'
+)
+
+criaCartao(
+    'portugues',
+    'qual o coletivo de porcos?',
+    'o coletivo de porcos é vara'
+)
+
+criaCartao(
+    'coisa inutil',
+    'qual igrediente mais estranho de um diamante ?',
+    'amendoim'
+)
